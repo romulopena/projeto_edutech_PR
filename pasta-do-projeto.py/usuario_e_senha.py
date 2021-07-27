141ff798ae8ed266c9fdc6930aa2534599baed45
romulopena <86451963+romulopena@users.noreply.github.com>
2021-07-27 16:36:23 -0300

    Delete pasta-do-projeto.py directory

--- pasta-do-projeto.py/usuario_e_senha.py
+++
@@ -1,8 +0,0 @@
-usuario = input("Informe o nome de usuário: ")
-senha = input("Informe a senha do usuário: ")
-while senha == usuario:
-    print("Senha não pode ser igual ao usuário!!!")
-    usuario = input("Informe o nome de usuário novamente: ")
-    senha = input("Informe a senha do usuário: ")
-    if senha != usuario:
-        print("USUÁRIO LOGADO COM SUCESSO!!!!")
