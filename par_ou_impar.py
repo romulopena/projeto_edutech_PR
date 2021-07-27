e89f395c61a02cdaa821f544b9c04f9525315e0e
romulopena <86451963+romulopena@users.noreply.github.com>
2021-07-27 15:51:24 -0300

    Arquivo de projeto programa par ou impar.
    Projeto do par ou impar criado com Python.

---
+++ par_ou_impar.py
@@ -0,0 +1,18 @@
+#JOGO DO PAR OU IMPAR
+
+#ENTRADA
+p = 0
+i = 0
+valor = float(input("Digite um número: "))
+
+#PROCESSAMENTO
+if (valor % 2 == 0):
+    p = valor
+    print(p )
+    print("Valor é PAR")
+else:
+    i = valor
+    print(i)
+    print("Valor é IMPAR")
+#SAÍDA
+
